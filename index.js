98adaa103c039c0eea9acbaa9b76dffdea88a0d6
rajmehta54 <rajmehta54@gmail.com>
2019-10-21 23:50:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays()
 
 function happyHolidaysTo(name)
 {
-  console.log('Happy holidays, ${name}!')
+  return 'Happy holidays, ${name}!'
 }
 
 function happyHolidayTo(holiday, name)
